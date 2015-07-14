a76cbcc1c61fcfd444210144ba091d1a0286b615
Dorab Patel <dorab@matchcraft.com>
2015-07-14 15:41:42 -0700

    Updated to Clojure 1.7.0.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/uswitch/adworj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/data.csv "0.1.2"]
                  [com.google.api-ads/ads-lib "1.39.0"]
                  [com.google.api-ads/adwords-axis "1.39.0"]
